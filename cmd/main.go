d48b5845c1c4b01acefcd82f0eba5efc5b4c9509
Sen-Com <sencommunis@protonmail.com>
2021-11-16 13:24:36 +0100

    Added config package and logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	log "github.com/sirupsen/logrus"
+	config "github.com/Sen-Com/inter-chain-arbitrage-bot/pkg/config"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	// Mock initialize config variables
+	// Should be done by the parser
+	config.Environment = "development"
+	config.LogLevel = "trace"
+
+	init_logger()
+}
+
+func init_logger() {
+	// Set logging format to json
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Activate/ Deactivate function specific reporting
+	switch config.Environment {
+	case "development":
+		log.SetReportCaller(true) // Adds significant overhead
+	case "production":
+		log.SetReportCaller(false)
+	}
+
+	// Set the logging level
+	switch config.LogLevel {
+	case "trace":
+		log.SetLevel(log.TraceLevel)
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	case "fatal":
+		log.SetLevel(log.FatalLevel)
+	case "panic":
+		log.SetLevel(log.PanicLevel)
+	}
 }
---
+++ pkg/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+// General
+var Environment string // production, development
+
+// Logging
+var LogLevel string // trace, debug, info, warn, error, fatal, panic
